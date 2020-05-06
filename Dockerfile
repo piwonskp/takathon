5209094dcb53f186ab466f6ae4fab9c16127019d
Piotr Piwoński <piwonskp>
2020-05-07 00:17:07 +0200

    Add setup.py and other packaging utils

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /usr/src/app
 
+RUN python setup.py sdist install
+
 CMD ["bash"]
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="takathon",
+    version="0.1",
+    author="Piotr Piwoński",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=["click>=7.1.2", "click-log>=0.3.2", "lark-parser>=0.8.5",],
+    entry_points="""
+        [console_scripts]
+        takathon=takathon.cli:takathon
+    """,
+)
